ac400c2bdbdd42b569071c0252b7eab649a246a0
yinm <yinm.cminor9@gmail.com>
2018-03-18 09:27:38 +0900

    Check XHR

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function asyncFunction() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve('Async Hello world')
-    }, 16)
+function getURL(URL) {
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest()
+    req.open('GET', URL, true)
+
+    req.onload = () => {
+      if (req.status === 200) {
+        resolve(req.responseText)
+      } else {
+        reject(new Error(req.statusText))
+      }
+    }
+
+    req.onerror = () => {
+      reject(new Error(req.statusText))
+    }
+
+    req.send()
   })
 }
 
-asyncFunction().then(function(value) {
+const URL = 'http://httpbin.org/get'
+getURL(URL).then(function onFulfilled(value) {
   console.log(value)
-}, function(error) {
+}).catch(function onRejected(error) {
   console.log(error)
 })
