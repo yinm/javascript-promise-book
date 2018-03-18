599ff427ea817a5cf3f15842b22942b753d906ac
yinm <yinm.cminor9@gmail.com>
2018-03-18 09:39:44 +0900

    Check creating promise object

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-function getURL(URL) {
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest()
-    req.open('GET', URL, true)
-
-    req.onload = () => {
-      if (req.status === 200) {
-        resolve(req.responseText)
-      } else {
-        reject(new Error(req.statusText))
-      }
-    }
-
-    req.onerror = () => {
-      reject(new Error(req.statusText))
-    }
-
-    req.send()
-  })
-}
-
-const url = 'http://httpbin.org/status/500'
-getURL(url).then(function onFulfilled(value) {
+promise = new Promise((resolve) => {
+  resolve(42);
+})
+promise.then((value) => {
   console.log(value)
-}).catch(function onRejected(error) {
-  console.error(error)
 })
