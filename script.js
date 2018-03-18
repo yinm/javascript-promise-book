fd9d0656b1e40904d738ac216a4d100b8eed999d
yinm <yinm.cminor9@gmail.com>
2018-03-18 11:09:28 +0900

    Check callback

--- script.js
+++ script.js
@@ -1,23 +1,72 @@
-const aPromise = new Promise((resolve) => {
-  resolve(100)
-})
-aPromise.then((value) => {
-  return value * 2
-})
-aPromise.then((value) => {
-  return value * 2
-})
-aPromise.then((value) => {
-  console.log('1: ' + value)
-})
+function getURLCallback(URL, callback) {
+  const req = new XMLHttpRequest()
+  req.open('GET', URL, true)
+  req.onload = () => {
+    if (req.status === 200) {
+      callback(null, req.responseText)
+    } else {
+      callback(new Error(req.statusText), req.response)
+    }
+  }
 
-const bPromise = new Promise((resolve) => {
-  resolve(100)
-})
-bPromise.then((value) => {
-  return value * 2
-}).then((value) => {
-  return value * 2
-}).then((value) => {
-  console.log('2: ' + value)
+  req.onerror = () => {
+    callback(new Error(req.statusText))
+  }
+
+  req.send()
+}
+
+// parse JSON with safety
+function jsonParse(callback, error, value) {
+  if (error) {
+    callback(error, value)
+  } else {
+    try {
+      const result = JSON.parse(value)
+      callback(null, result)
+    } catch(e) {
+      callback(e, value)
+    }
+  }
+}
+
+// request with XHR
+const request = {
+  comment: function getComment(callback) {
+    return getURLCallback('http://azu.github.io/promises-book/json/comment.json', jsonParse.bind(null, callback))
+  },
+  people: function getPeople(callback) {
+    return getURLCallback('http://azu.github.io/promises-book/json/people.json', jsonParse.bind(null, callback))
+  }
+}
+
+// call callback functions when all XHR requests end
+function allRequest(requests, callback, results) {
+  if (requests.length === 0) {
+    return callback(null, results)
+  }
+
+  const req = requests.shift()
+  req((error, value) => {
+    if (error) {
+      callback(error, value)
+    } else {
+      results.push(value)
+      allRequest(requests, callback, results)
+    }
+  })
+}
+
+function main(callback) {
+  allRequest([request.comment, request.people], callback, [])
+}
+
+// example
+main((error, results) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+
+  console.log(results)
 })
